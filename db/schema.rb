1ae885730e209e73ed4a1eac8f4830c70f49b13c
Roma Gusyev <galiyin@gmail.com>
2019-05-28 19:38:05 -0400

    commit updates

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20190524212531) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.text "comment"
